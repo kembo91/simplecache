4d072a2a84398ef69a9629b61f4b9617f667a504
Гали-Кетема Мбого <gali-ketem.mbogo@sbermarket.ru>
2022-11-12 01:05:29 +0300

    wrap as package and add readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# Simple generic in-memory cache
+This is a generic key-value store, written in pure go
+
+You can set any `comparable` type `K` as key. And `any` type `V` as value
+
+Concurrent reads and writes are synchronized under the hood via channels.
+
+There is also a TTL cache with +- 1 second accuracy
+### How to use
+```go
+c := simplecache.NewCache[string, int]()
+defer c.Close()
+// For TTL use c := simplecache.NewTTLCache[K, D](ttl time.Duration)
+c.Set("foo", 100)
+got, ok := c.Get("foo")
+if ok {
+    fmt.Println(got) // 100
+}
+```
+### How to install
+```
+go install github.com/kembo91/simplecache
+```
--- main.go
+++ main.go
@@ -1,89 +1,51 @@
-package main
+package smiplecache
 
 import (
 	"context"
-	"fmt"
-	"strconv"
-	"sync"
 	"time"
 )
 
-type Cache[K comparable, D any] interface {
-	Get(key K) (D, bool)
-	Set(key K, value D)
+type Cache[K comparable, V any] interface {
+	Get(key K) (V, bool)
+	Set(key K, value V)
 	Delete(key K)
 	Close()
 }
 
-type dataWrapper[K comparable, D any] struct {
+type dataWrapper[K comparable, V any] struct {
 	key      K
-	value    D
+	value    V
 	deleteAt time.Time
 }
 
-type readType[K comparable, D any] struct {
-	returnChan chan *D
+type readType[K comparable, V any] struct {
+	returnChan chan *V
 	key        K
 }
 
-type writeType[K comparable, D any] struct {
+type writeType[K comparable, V any] struct {
 	key   K
-	value D
+	value V
 }
 
-type cache[K comparable, D any] struct {
+type cache[K comparable, V any] struct {
 	ctx         context.Context
-	data        map[K]*D
-	orderedData []*dataWrapper[K, D]
-	writeChan   chan writeType[K, D]
-	readChan    chan readType[K, D]
+	data        map[K]*V
+	orderedData []*dataWrapper[K, V]
+	writeChan   chan writeType[K, V]
+	readChan    chan readType[K, V]
 	deleteChan  chan K
 	ttl         time.Duration
 	cancel      context.CancelFunc
 }
 
-func NewCache[K comparable, D any](ttl time.Duration) Cache[K, D] {
-	ctx, cancel := context.WithCancel(context.Background())
-	rv := &cache[K, D]{
-		data:        make(map[K]*D),
-		writeChan:   make(chan writeType[K, D]),
-		ttl:         ttl,
-		readChan:    make(chan readType[K, D]),
-		deleteChan:  make(chan K),
-		ctx:         ctx,
-		cancel:      cancel,
-		orderedData: make([]*dataWrapper[K, D], 0),
-	}
-	go func() {
-		defer func() {
-			close(rv.readChan)
-			close(rv.writeChan)
-			close(rv.deleteChan)
-		}()
-		for {
-			select {
-			case read := <-rv.readChan:
-				rv, ok := rv.data[read.key]
-				if !ok {
-					read.returnChan <- nil
-					continue
-				}
-				read.returnChan <- rv
-			case write := <-rv.writeChan:
-				dr := &dataWrapper[K, D]{
-					deleteAt: time.Now().Add(rv.ttl),
-					key:      write.key,
-					value:    write.value,
-				}
-				rv.data[write.key] = &write.value
-				rv.orderedData = append(rv.orderedData, dr)
-			case del := <-rv.deleteChan:
-				delete(rv.data, del)
-			case <-rv.ctx.Done():
-				return
-			}
-		}
-	}()
+func NewCache[K comparable, V any]() Cache[K, V] {
+	return newCache[K, V]()
+}
+
+func NewTTLCache[K comparable, V any](ttl time.Duration) Cache[K, V] {
+	rv := newCache[K, V]()
+	rv.ttl = ttl
 	go func() {
 		tick := time.NewTicker(time.Second)
 		defer tick.Stop()
@@ -106,62 +68,71 @@ func NewCache[K comparable, D any](ttl time.Duration) Cache[K, D] {
 	return rv
 }
 
-func (c *cache[K, D]) Get(key K) (D, bool) {
-	retc := make(chan *D)
+func newCache[K comparable, V any]() *cache[K, V] {
+	ctx, cancel := context.WithCancel(context.Background())
+	rv := &cache[K, V]{
+		data:        make(map[K]*V),
+		writeChan:   make(chan writeType[K, V]),
+		readChan:    make(chan readType[K, V]),
+		deleteChan:  make(chan K),
+		ctx:         ctx,
+		cancel:      cancel,
+		orderedData: make([]*dataWrapper[K, V], 0),
+	}
+	go func() {
+		defer func() {
+			close(rv.readChan)
+			close(rv.writeChan)
+			close(rv.deleteChan)
+		}()
+		for {
+			select {
+			case read := <-rv.readChan:
+				rv, ok := rv.data[read.key]
+				if !ok {
+					read.returnChan <- nil
+					continue
+				}
+				read.returnChan <- rv
+			case write := <-rv.writeChan:
+				dr := &dataWrapper[K, V]{
+					deleteAt: time.Now().Add(rv.ttl),
+					key:      write.key,
+					value:    write.value,
+				}
+				rv.data[write.key] = &write.value
+				rv.orderedData = append(rv.orderedData, dr)
+			case del := <-rv.deleteChan:
+				delete(rv.data, del)
+			case <-rv.ctx.Done():
+				return
+			}
+		}
+	}()
+	return rv
+}
+
+func (c *cache[K, V]) Get(key K) (V, bool) {
+	retc := make(chan *V)
 	defer close(retc)
-	rc := readType[K, D]{returnChan: retc, key: key}
+	rc := readType[K, V]{returnChan: retc, key: key}
 	c.readChan <- rc
 	got := <-rc.returnChan
 	if got == nil {
-		dd := new(D)
+		dd := new(V)
 		return *dd, false
 	}
 	return *got, true
 }
 
-func (c *cache[K, D]) Set(key K, value D) {
-	c.writeChan <- writeType[K, D]{key: key, value: value}
+func (c *cache[K, V]) Set(key K, value V) {
+	c.writeChan <- writeType[K, V]{key: key, value: value}
 }
 
-func (c *cache[K, D]) Delete(key K) {
+func (c *cache[K, V]) Delete(key K) {
 	c.deleteChan <- key
 }
 
-func (c *cache[K, D]) Close() {
+func (c *cache[K, V]) Close() {
 	c.cancel()
-}
-
-func main() {
-	cache := NewCache[string, string](time.Second * 3)
-	cache.Set("hello", "world")
-	got, ok := cache.Get("hello")
-	if ok {
-		fmt.Println(got)
-	} else {
-		fmt.Println("something went wrong")
-	}
-	wg := sync.WaitGroup{}
-	for i := 0; i < 1000; i++ {
-		go func(i int) {
-			defer wg.Done()
-			wg.Add(1)
-			cache.Set(strconv.Itoa(i), "yoyo")
-		}(i)
-	}
-	wg.Wait()
-	for i := 0; i < 1000; i++ {
-		go func(i int) {
-			defer wg.Done()
-			wg.Add(1)
-			res, ok := cache.Get(strconv.Itoa(i))
-			if !ok {
-				fmt.Println("not ok")
-			}
-			if res != "yoyo" {
-				fmt.Println("not yoyo")
-			}
-		}(i)
-	}
-	wg.Wait()
-	cache.Close()
 }
